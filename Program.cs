5d60e240dfdb70fdb43fde34dadbbe69c503ebdd
Yana Abramenko <yanabrle@gmail.com>
2026-03-19 16:45:06 +0200

    Implement sequential solution and timing

--- Program.cs
+++ Program.cs
@@ -1,9 +1,46 @@
-﻿namespace Lab2;
+﻿using System;
+using System.Diagnostics;
 
-class Program
+internal static class Program
 {
-    static void Main(string[] args)
+    private static void FindMaxSeq(int[] arr)
     {
-        Console.WriteLine("Hello, World!");
+        int max = int.MinValue;
+        int count = 0;
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+                count = 1;
+            }
+            else if (arr[i] == max)
+            {
+                count++;
+            }
+        }
+
+        sw.Stop();
+
+        Console.WriteLine($"seq:\tmax: {max}\tcount: {count}\ttime: {sw.Elapsed.TotalSeconds:F6}");
+    }
+
+    private static void Main()
+    {
+        Random rnd = new Random();
+
+        int n = 1_000_000;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = rnd.Next(0, 100000);
+        }
+
+        Console.WriteLine($"array length: {n}");
+        FindMaxSeq(arr);
     }
 }
